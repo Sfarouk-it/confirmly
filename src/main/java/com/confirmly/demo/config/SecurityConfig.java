3ecb53524354b2b0af32dc7fe8bc6e032b781561
farouk <farouk2003s@gmail.com>
2025-10-04 02:45:39 +0100

    facbook auth

--- src/main/java/com/confirmly/demo/config/SecurityConfig.java
+++ src/main/java/com/confirmly/demo/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
         http
             .csrf().disable() // disable CSRF for API (optional if using JWT)
             .authorizeHttpRequests()
-            .requestMatchers("/api/authantification/**", "/metaHookController/**").permitAll() // allow signup & login
+            .requestMatchers("/api/authantification/**", "/metaHookController/**", "/api/platformsAuth/**").permitAll() // allow signup & login
             .anyRequest().authenticated() // protect everything else
             .and()
             .formLogin(); // optional: default form login
