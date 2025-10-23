5734d76bab9feac560eb18ddb13817ed480724b9
farouk <farouk2003s@gmail.com>
2025-10-23 14:55:51 +0100

    fix a little problem

--- src/main/java/com/confirmly/demo/Repositories/FacebookAccountRepository.java
+++ src/main/java/com/confirmly/demo/Repositories/FacebookAccountRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.confirmly.demo.model.FacebookAccount;
 
 public interface FacebookAccountRepository extends JpaRepository<FacebookAccount, Long> {
-    Optional<FacebookAccount> findByUserId(Long userId);
     Optional<FacebookAccount> findByFacebookId(String facebookId);
 }
