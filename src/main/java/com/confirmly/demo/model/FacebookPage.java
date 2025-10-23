94ada969e2bd1c49cdeb4acf6fec7bb1fc645cd4
farouk <farouk2003s@gmail.com>
2025-10-23 14:34:57 +0100

    a new update

--- src/main/java/com/confirmly/demo/model/FacebookPage.java
+++ src/main/java/com/confirmly/demo/model/FacebookPage.java
@@ -1,10 +1,14 @@
 package com.confirmly.demo.model;
 
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "facebook_page")
 public class FacebookPage {
     
     @Id
